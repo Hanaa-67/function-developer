83a4cc3d5709799fe69fc095ffe9871190bde0a9
Hanaa-67 <hanaa.elsehemy64@gmail.com>
2024-12-18 02:57:56 +0300

    first commit

--- js/Famms.js
+++ js/Famms.js
@@ -1,5 +1,5 @@
 // navbar
-$('.navList li a').click(function(){
+$('.navList li a .nav-link').click(function(){
     $('.activ').removeClass('activ')
     $(this).addClass('activ');
     $('.dropdown-menu').removeClass('appear')
@@ -15,15 +15,83 @@ $('.pages').on('click',function(){
 })
 
 
+// collapse bar
+$('.side-bar').click(function(){
+    $('.navList').toggleClass('active')
+})
+
+
+// slider 1
+function nextSlider(){
+    let activeSlide = $('.slides.active');
+    let activePoint = $('.point.light')
+
+    if(activeSlide.next().length>0){
+        activeSlide.removeClass('active')
+        activeSlide.next().addClass('active')
+        activePoint.removeClass('light');
+        activePoint.next().addClass('light')
+
+    }else{
+        activeSlide.removeClass('active')
+        $('.slides').first().addClass('active')
+        activePoint.removeClass('light')
+        $('.point').first().addClass('light')
+    }
+
+}
+
+setInterval(function(){
+    nextSlider()
+},2000) 
+
+$('.point').click(function(){
+    let order = $(this).data('order')
+    let activeSlide = $('.slides.active');
+    let activePoint = $('.point.light')
+
+    activePoint.removeClass('light');
+    $(this).addClass('light');
+    activeSlide.removeClass('active')
+    $('.slides').eq(order).addClass('active')
+})
+
+// testimonial slider
+
+function nextTestimonial(){
+    let activeSlide = $('.testi-slider.active')
+    if(activeSlide.next().length>0){
+        activeSlide.removeClass('active')
+        activeSlide.next().addClass('active')
+    }else{
+        activeSlide.removeClass('active')
+        $('.testi-slider').first().addClass('active')
+    }
+}
+function prevTestimonial(){
+    let activeSlide = $('.testi-slider.active')
+    if(activeSlide.prev().length>0){
+        activeSlide.removeClass('active')
+        activeSlide.prev().addClass('active')
+    }else{
+        activeSlide.removeClass('active')
+        $('.testi-slider').last().addClass('active')
+    }
+}
+
+$('.arrow-right').on('click',function(){
+    nextTestimonial()
+})
+$('.arrow-left').on('click',function(){
+    prevTestimonial()
+})
+// setInterval(()=>{
+//     nextTestimonial()
+// },2000)
 
 
 
 
-// targetting section
-// $('.navList li a').click(function(e){
-    
-
-// })
 // ajax request
 $.ajax({
     url:"https://dummyjson.com/products",
